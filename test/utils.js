a40f11686c2968c701eb31d9fadab6c4c1eabd52
Emiliano Bovetti <emiliano.bovetti@gmail.com>
2018-12-07 12:42:00 +0100

    test: use `lodash.isequal`

--- test/utils.js
+++ test/utils.js
@@ -1,38 +1,11 @@
-const { is, pipe, first, slice, map, reduce, failWith } = require('../src/index.mjs');
-
-const forAll = (vals, fn) =>
-  vals.reduce((acc, val, idx) => acc && fn(val, idx), true);
-
-const objectToArrayPair = obj =>
-  Object.keys(obj)
-    .sort()
-    .map(key => [ key, obj[key] ]);
-
-const isObjectEqual = (v1, v2) =>
-  is.obj(v1) &&
-  is.obj(v2) &&
-  pipe([ v1, v2 ])
-    .into(map, objectToArrayPair)
-    .and(([ o1, o2 ]) => isArrayEqual(o1, o2))
-    .result;
-
-const isArrayEqual = (v1, v2) =>
-  is.array.like(v1) &&
-  is.array.like(v2) &&
-  v1.length === v2.length &&
-  forAll(v1, (val, idx) => isEqual(val, v2[idx]));
-
-const isEqual = (v1, v2) =>
-  v1 === v2 ||
-  isNaN(v1) && isNaN(v2) ||
-  isArrayEqual(v1, v2) ||
-  isObjectEqual(v1, v2);
+const { is, pipe, first, reduce, failWith } = require('../src/index.mjs');
+const isEqual = require('lodash.isequal');
 
 const toString = val =>
   is.array(val)
-  ? `[ ${val.map(toString).join(', ')} ]`
+  ? `[${val.map(toString).join(',')}]`
   : is.obj(val)
-  ? `{ ${objectToArrayPair(val).map(([ k, v ]) => `${k}: ${toString(v)}`)} }`
+  ? val.toSource()
   : String(val);
 
 const eqReducer = ([ areEquals, last ], current) =>
@@ -51,6 +24,9 @@ const compare = (...args) => {
 
   return areEquals || failWith(new Error(`compare(${args.map(toString)}) failed`));
 };
+
+const forAll = (vals, fn) =>
+  vals.reduce((acc, val, idx) => acc && fn(val, idx), true);
 
 const values = [
   undefined,
